e27e6ad43099b462726658fea4ad9479e4baf367
Sadie Allen <sadiela@Sadies-MacBook-Pro.local>
2022-05-03 23:09:36 -0400

    config

--- app.py
+++ app.py
@@ -179,6 +179,6 @@ def create_playlist(user):
     return playlist_create['id']
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))     
+    port = int(os.environ.get("PORT", 33507))     
     app.run(host='0.0.0.0', port=port, debug=True)
     #app.run()
