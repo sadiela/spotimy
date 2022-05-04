131f396f6f23f5eb72ac2b620767681d40dd257a
Sadie Allen <sadiela@Sadies-MacBook-Pro.local>
2022-05-03 23:13:15 -0400

    include build folder

---
+++ spotify-playlist-frontend/build/precache-manifest.fe4fe525f375d3de8bb753240d3a6005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98e70a3f9dbca792e481",
+    "url": "/static/css/main.58655310.chunk.css"
+  },
+  {
+    "revision": "98e70a3f9dbca792e481",
+    "url": "/static/js/main.98e70a3f.chunk.js"
+  },
+  {
+    "revision": "66c154a22e2b7b2b9b37",
+    "url": "/static/js/1.66c154a2.chunk.js"
+  },
+  {
+    "revision": "3a9dc0eb1c6d13e203d3",
+    "url": "/static/js/2.3a9dc0eb.chunk.js"
+  },
+  {
+    "revision": "ae53f87981aff75462ae",
+    "url": "/static/js/runtime~main.ae53f879.js"
+  },
+  {
+    "revision": "3cae33dcadcb3c37a77e337e98b92276",
+    "url": "/index.html"
+  }
+];
