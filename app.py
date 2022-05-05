2d48b30d59de7a03ac237f9f5bf20ff38e855b8f
Sadie Allen <sadiela@Sadies-MacBook-Pro.local>
2022-05-05 13:54:30 -0400

    test

--- app.py
+++ app.py
@@ -167,6 +167,6 @@ def create_playlist(user, token):
     return playlist_return
 
 if __name__ == '__main__':
-    #port = int(os.environ.get("PORT", 33507))     
-    #app.run(host='0.0.0.0', port=port, debug=True)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 33507))     
+    app.run(host='0.0.0.0', port=port, debug=True)
+    #app.run(debug=True)
